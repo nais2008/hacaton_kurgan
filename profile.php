3119c34f4b9e80281ad499099a9e75834928b039
IrinaKoliasnikova <150648817+IrinaKoliasnikova@users.noreply.github.com>
2023-11-14 10:34:33 +0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -21,7 +21,7 @@ if($conn->connect_error){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SUPER GEET</title>
+    <title>SUPER GEEK</title>
 
     <!-- CSS -->
 
